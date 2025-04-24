5faa74bb0fbcc3265e71d7a28e769893d31d61bf
HarishN1802 <harishnamann@shriramsoftwares.com>
2025-04-24 13:24:25 +0530

    Fixed a bug in product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const authMiddleware= require("../middleware/auth");
 
-router.post("/add",authMiddleware,productController.createProduct);
+router.post("/add",productController.createProduct);
 router.get("/all",productController.getAllProducts);
 router.get("/:id",productController.getProductById);
-router.put("/update/:id",authMiddleware,productController.updateProduct);
+router.put("/update/:id",productController.updateProduct);
 router.delete("/delete/:id",authMiddleware,productController.deleteProduct);
 router.get("/category/:categoryName",productController.getProductsByCategory);
 
